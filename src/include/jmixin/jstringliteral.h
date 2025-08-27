220a2e972ec853efb2c6ae9601d45f64570c0e8e
Jefferson Ferreira <jeffersonfr@gmail.com>
2025-08-27 09:06:28 -0300

    Add jstringliteral

---
+++ src/include/jmixin/jstringliteral.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+
+namespace jmixin {
+  template<size_t N>
+  struct StringLiteral {
+  public:
+    constexpr StringLiteral(const char (&str)[N]) { std::copy_n(str, N, value); }
+
+    constexpr std::string to_string() const { return std::string{value, N - 1}; }
+
+    char value[N];
+  };
+}
